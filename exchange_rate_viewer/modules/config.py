f7ade73a6f670b542f04963cc99fdc247458d2e9
wieczorekgrzegorz <124365703+wieczorekgrzegorz@users.noreply.github.com>
2024-03-30 11:33:01 +0100

    [FIX] Update logging configuration to use WARNING level and replace log file path variable

--- exchange_rate_viewer/modules/config.py
+++ exchange_rate_viewer/modules/config.py
@@ -1,4 +1,5 @@
 """Configuration module for the exchange rate viewer application."""
+
 import logging
 import logging.config
 import os
@@ -10,11 +11,8 @@ from modules import sqldb_communication
 
 log = logging.getLogger(name="app_logger")
 
-DB_DIR_PATH = os.environ["DB_DIR_PATH"]
 DB_FILEPATH = os.environ["DB_FILEPATH"]
-LOGS_DIR_PATH = os.environ["LOGS_DIR_PATH"]
 LOGS_FILEPATH = os.environ["LOGS_FILEPATH"]
-CHART_DIR_PATH = os.environ["CHART_DIR_PATH"]
 CHART_FILEPATH = os.environ["CHART_FILEPATH"]
 LOGGING_CONFIG_FILEPATH = os.environ["LOGGING_CONFIG_FILEPATH"]
 
@@ -25,10 +23,27 @@ MAX_DATE_RANGE = 93  # Maximum range of days allowed by NBP API
 REQUEST_TIMEOUT = 60
 
 
-def setup_logging() -> None:
-    """Set up logging configuration."""
+def load_logging_config() -> dict:
+    """Load logging configuration from a file."""
     with open(file=LOGGING_CONFIG_FILEPATH, mode="r", encoding="utf-8") as f:
         config = yaml.safe_load(stream=f.read())
+
+    return config
+
+
+def replace_log_filepath(config: dict) -> None:
+    """Replace ${LOGS_FILEPATH} in the logging config."""
+    for handler in config["handlers"].values():
+        if "filename" in handler and "${LOGS_FILEPATH}" in handler["filename"]:
+            handler["filename"] = os.getenv(key="LOGS_FILEPATH")
+
+
+def setup_logging() -> None:
+    """Set up logging configuration."""
+    config = load_logging_config()
+
+    replace_log_filepath(config=config)
+
     logging.config.dictConfig(config=config)
 
 
